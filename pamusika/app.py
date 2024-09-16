bece42478e280975cf97f46037f06142b42c8603
MUFARO CONEL NYAKUDYA <nyakudyamufa2002@gmail.com>
2024-09-16 21:44:52 +0200

    dev server

--- pamusika/app.py
+++ pamusika/app.py
@@ -375,7 +375,12 @@ class GroupAPI(MethodView):
         
 app.add_url_rule('/webhook', view_func=GroupAPI.as_view('wa_webhook'))
 
+# if __name__ == "__main__":
+    # app.run(debug=True)
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
     with app.app_context():
             db.create_all()
